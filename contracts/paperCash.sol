2ac2c25cae1a74d3decc20ea42ca56099e3e9acf
Ivo Georgiev <ivo@linvo.org>
2017-08-19 17:55:43 +0300

    grant can only be claimed once

--- contracts/paperCash.sol
+++ contracts/paperCash.sol
@@ -4,18 +4,28 @@ pragma solidity ^0.4.13;
 
 contract paperCash {
 	mapping (bytes32 => uint) grants;
+	mapping (bytes32 => bool) claimed;
 
 	function createGrant(bytes32 _hashedKey)
 		payable
 	{
+		require(grants[_hashedKey] == 0);
+		require(claimed[_hashedKey] == false);
+		
 		require(msg.value > 0);
 		grants[_hashedKey] = msg.value;
 	}
 
 	function claimGrant(bytes32 _key) 
 	{
-		uint grant = grants[sha3(_key)];
+		bytes32 hashedKey = sha3(_key);
+
+		require(!claimed[hashedKey]);
+		claimed[hashedKey] = true;
+
+		uint grant = grants[hashedKey];
 		require(grant > 0);
+
 		require(msg.sender.send(grant));
 	}
 }
