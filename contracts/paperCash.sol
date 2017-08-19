324834c414fc5e00861bf57069c024bca87fca26
Ivo Georgiev <ivo@linvo.org>
2017-08-19 17:50:29 +0300

    contract complete

--- contracts/paperCash.sol
+++ contracts/paperCash.sol
@@ -1,18 +1,21 @@
 pragma solidity ^0.4.13;
 
-import "../zeppelin-solidity/contracts/ownership/Ownable.sol";
+//import "../zeppelin-solidity/contracts/ownership/Ownable.sol";
 
-contract paperCash is Ownable {
+contract paperCash {
 	mapping (bytes32 => uint) grants;
 
 	function createGrant(bytes32 _hashedKey)
 		payable
 	{
-		
+		require(msg.value > 0);
+		grants[_hashedKey] = msg.value;
 	}
 
 	function claimGrant(bytes32 _key) 
 	{
-
+		uint grant = grants[sha3(key)];
+		require(grant > 0);
+		require(msg.sender.send(grant));
 	}
 }
