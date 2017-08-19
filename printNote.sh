b664935086c7fce2289b3232cb8baff85cfe357c
Ivo Georgiev <ivo@linvo.org>
2017-08-19 23:26:26 +0300

    printNote finished

--- printNote.sh
+++ printNote.sh
@@ -16,3 +16,7 @@ echo $QRCODE
 
 curl "$QRCODE" > /tmp/qr.png
 
+composite -dissolve 60% /tmp/qr.png -gravity center ./notes/rollsafe.jpg  /tmp/banknote.jpg
+convert /tmp/banknote.jpg -gravity South -background YellowGreen -splice 0x18 -annotate +0+2 '$2 ETH' /tmp/banknote.jpg
+
+echo /tmp/banknote.jpg
