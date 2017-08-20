760897e2741a3bbc82c898c7cce0983ff6bff052
Ivo Georgiev <ivo@linvo.org>
2017-08-20 18:22:30 +0300

    add complex grants

--- contracts/paperCash.sol
+++ contracts/paperCash.sol
@@ -3,17 +3,39 @@ pragma solidity ^0.4.13;
 //import "../zeppelin-solidity/contracts/ownership/Ownable.sol";
 
 contract paperCash {
-	mapping (bytes32 => uint) grants;
+	struct Grant {
+		uint amount;
+		uint whenWithdrawable;
+		address allowedWithdrawee;
+	}
+
+	mapping (bytes32 => Grant) grants;
 	mapping (bytes32 => bool) claimed;
 
 	function createGrant(bytes32 _hashedKey)
 		payable
 	{
-		require(grants[_hashedKey] == 0);
+		require(grants[_hashedKey].amount == 0);
 		require(claimed[_hashedKey] == false);
 
 		require(msg.value > 0);
-		grants[_hashedKey] = msg.value;
+
+		grants[_hashedKey].amount = msg.value;
+
+		LogGrantCreated(_hashedKey, msg.value);
+	}
+
+	function createComplexGrant(bytes32 _hashedKey, uint _whenWithdrawable, address _allowedWithdrawee)
+		payable
+	{
+		require(grants[_hashedKey].amount == 0);
+		require(claimed[_hashedKey] == false);
+
+		require(msg.value > 0);
+
+		grants[_hashedKey].amount = msg.value;
+		grants[_hashedKey].whenWithdrawable = _whenWithdrawable;
+		grants[_hashedKey].allowedWithdrawee = _allowedWithdrawee;
 
 		LogGrantCreated(_hashedKey, msg.value);
 	}
@@ -25,19 +47,22 @@ contract paperCash {
 		require(!claimed[hashedKey]);
 		claimed[hashedKey] = true;
 
-		uint amount = grants[hashedKey];
-		require(amount > 0);
+		var grant = grants[hashedKey];
 
-		require(msg.sender.send(amount));
+		require(grant.amount > 0);
+		if (grant.whenWithdrawable != 0) require(now > grant.whenWithdrawable);
+		if (grant.allowedWithdrawee != 0) require(msg.sender == grant.allowedWithdrawee);
 
-		LogGrantClaimed(hashedKey, amount);
+		require(msg.sender.send(grant.amount));
+
+		LogGrantClaimed(hashedKey, grant.amount);
 	}
 
 	function isGrantValid(bytes32 _hashedKey)
 		constant
 		returns (bool)
 	{
-		return (grants[_hashedKey] > 0 && !claimed[_hashedKey]);
+		return (grants[_hashedKey].amount > 0 && !claimed[_hashedKey]);
 	}
 
 	event LogGrantCreated(bytes32 hashedKey, uint amount);
